d832142f42d2faf7c7cda87d65fef6239300c687
Dsgathebest <gomezabrildaniel1234@gmail.com>
2023-06-09 12:31:38 -0500

    Intento de subir los archivos al Git

---
+++ app.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once 'vendor/autoload.php';
+use App\Database;
+$db = new Database();
+$conn = $db->getConnection('mysql');
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'apolm01-10/php-mysql-pdp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b449432000bb689ef426a1d28c19c45b174ce762',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'apolm01-10/php-mysql-pdp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b449432000bb689ef426a1d28c19c45b174ce762',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
